bead0da60efb64bad9e48a07b3a13558c5cacc4c
Mark Parking <108267077+markparking@users.noreply.github.com>
2023-06-23 14:04:29 +0200

    Add files via upload

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Technical Education Copenhagen")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © Technical Education Copenhagen 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ad28aea-502c-44f4-b4c4-544a38b1eefa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Kørselsbidrag;
+
+namespace Kørselsbidrag
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void Test20Km()
+        {
+            // Arrange
+            Logic logic = new Logic();
+            logic.kmPerDag = 20;
+            logic.broType = 0;
+
+            // Act
+            double result = logic.Output();
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test25Km()
+        {
+            // Arrange
+            Logic logic = new Logic();
+            logic.kmPerDag = 25;
+            logic.broType = 0;
+
+            // Act
+            double result = logic.Output();
+
+            // Assert
+            Assert.AreEqual(2.16, result);
+        }
+        [TestMethod]
+        public void TestMinus50Km()
+        {
+            // Arrange
+            Logic logic = new Logic();
+            logic.kmPerDag = -50;
+            logic.broType = 0;
+
+            // Act
+            double result = logic.Output();
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Test100Km()
+        {
+            // Arrange
+            Logic logic = new Logic();
+            logic.kmPerDag = 100;
+            logic.broType = 0;
+
+            // Act
+            double result = logic.Output();
+
+            // Assert
+            Assert.AreEqual(164, 16, result);
+        }
+        [TestMethod]
+        public void Test120Km()
+        {
+            // Arrange
+            Logic logic = new Logic();
+            logic.kmPerDag = 120;
+            logic.broType = 0;
+
+            // Act
+            double result = logic.Output();
+
+            // Assert
+            Assert.AreEqual(207,36, result);
+        }
+        [TestMethod]
+        public void Test200Km()
+        {
+            // Arrange
+            Logic logic = new Logic();
+            logic.kmPerDag = 200;
+            logic.broType = 0;
+
+            // Act
+            double result = logic.Output();
+
+            // Assert
+            Assert.AreEqual(293.76, result);
+        }
+    }
+}
