2c098aab03ce1be3de193008f366c515e716f189
mp <mp@PROELE94>
2023-06-23 10:24:33 +0200

    Add project files.

---
+++ Logic.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kørselsbidrag
+{
+    public class Logic
+    {
+        public Logic()
+        {
+            //Input();
+        }
+
+        public double kmPerDag { get; set; }
+        public double kmMin { get; set; }
+        public int broType { get; set; }
+
+        public string Input()
+        {
+
+            return "Jeg har gjort noget dumt";
+        }
+
+        public double Output()
+        {
+            double bropenge = BroPenge(broType);
+            double penge = 0;
+
+            if (kmPerDag > 120)
+            {
+                double temp = kmPerDag - 120;
+                penge = temp * 1.08;
+                kmPerDag -= temp;
+            }
+
+            if (kmPerDag > 24)
+            {
+                double temp = kmPerDag - 24;
+                penge += temp * 2.16;
+                kmPerDag -= temp;
+            }
+
+            if ((kmPerDag - 24) * 2.16 + penge + bropenge > 0)
+            {
+                double result = (kmPerDag - 24) * 2.16 + penge + bropenge;
+                Console.WriteLine(result);
+                return result;
+            }
+            else
+            {
+                double output = 0;
+                Console.WriteLine(output);
+                return output;
+            }
+        }
+        public double BroPenge(int broType)
+        {
+
+            if (broType == 0)
+            {
+                return 0;
+            }
+
+            if (broType == 1)
+            {
+                return 50 * 2;
+            }
+
+            return 110 * 2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kørselsbidrag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic logic = new Logic();
+            Console.WriteLine("Km per dag:");
+            logic.kmPerDag = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("[0] Ingen bro\n[1] Øresund\n[2] Storebelt");
+            logic.broType = Convert.ToInt32(Console.ReadLine());
+            logic.Output();
+                Console.ReadKey();
+        }
+
+    }
+}
